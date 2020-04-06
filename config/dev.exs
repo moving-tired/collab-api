ca0b80cd9921b4b983379cd9b6ed5ea8a67c5f86
Andrey Justo <andrey.justo@gmail.com>
2020-04-05 23:25:15 -0300

    change pool

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ config :collab, Collab.Repo,
   database: "collab_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 2
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
