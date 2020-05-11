8fb4d6508a5b2c08f97fb66cd0e8086f7f18aea1
Andrey Justo <andrey.justo@gmail.com>
2020-05-11 10:01:59 -0300

    fix credo syntax

--- lib/collab/user/update_user.ex
+++ lib/collab/user/update_user.ex
@@ -8,15 +8,13 @@ defmodule Collab.UpdateUser do
   alias Collab.{Repo, UserRepo}
 
   def run(id, params) do
-    try do
-      UserRepo.get_user!(id)
-      |> cast(params, [:id, :name, :email, :password, :phone, :birthday])
-      |> validate_format(:email, ~r/@/)
-      |> put_password()
-      |> Repo.update()
-    rescue
-      Ecto.NoResultsError -> nil
-    end
+    UserRepo.get_user!(id)
+    |> cast(params, [:id, :name, :email, :password, :phone, :birthday])
+    |> validate_format(:email, ~r/@/)
+    |> put_password()
+    |> Repo.update()
+  rescue
+    Ecto.NoResultsError -> nil
   end
 
   defp put_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
