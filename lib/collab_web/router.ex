1ce2b5a5bf6b1ab92cb1a6adaafef46d41c0cd2e
Andrey Justo <andrey.justo@gmail.com>
2020-05-11 09:51:44 -0300

    fix slash into beginning for routers

--- lib/collab_web/router.ex
+++ lib/collab_web/router.ex
@@ -10,7 +10,7 @@ defmodule CollabWeb.Router do
     pipe_through :api
     # pending update
     resources "/users", User.UserController, except: [:new, :index, :edit, :update]
-    put("users/:id", User.UserController, :update)
+    put("/users/:id", User.UserController, :update)
     post("/users/sign_in", User.SessionController, :login)
 
     resources "/tasks", Task.TaskController, except: [:new, :index, :edit, :update]
